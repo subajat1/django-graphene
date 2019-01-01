c6054168214c3b90018fce6d9d0ff7168d598718
subajat1 <bayumunajat@outlook.com>
2019-01-01 13:32:11 +0800

    improve using django-filter 1.1

--- src/manager/schema.py
+++ src/manager/schema.py
@@ -1,13 +1,17 @@
 import graphene
 from graphene_django.types import DjangoObjectType
+from graphene_django.filter.fields import DjangoFilterConnectionField
+
 from .models import Message
 
 class MessageType(DjangoObjectType):
     class Meta:
         model = Message
+        filter_fields = {'message': ['icontains']}
+        interfaces = (graphene.Node, )
 
-class Query(graphene.ObjectType):
-    all_messages_man = graphene.List(MessageType)
+class Query(graphene.AbstractType):
+    all_messages = DjangoFilterConnectionField(MessageType)
 
-    def resolve_all_messages_man(self, info):
+    def resolve_all_messages(self, info):
         return Message.objects.all()
